f99412c719adf99297ea41e8603edd517949ba5e
”abdelrahman <eabdo6990@gmail.com>
2026-01-06 20:27:50 +0200

    photo model

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ import path from 'node:path'
 import * as dotenv from 'dotenv'
 dotenv.config({path:path.join(path.resolve(),'./src/config/.env.dev')})
 
+import multer from 'multer'
+const upload=multer({dest:'uploads'}) 
+
+
+
 import express from "express"
 import bootstarb from "./src/app.controller.js"
 const app =express()
 const port=process.env.PORT||4000
 bootstarb(app,express)
+
+
+app.post('/photos',upload.single('photo'),(req,res,next)=>{
+
+    
+    res.json({message:"success"})
+
+})
 app.listen(port,()=>{
     console.log("server is running on port 3000")
 })
--- src/db/model/photo.model.js
+++ src/db/model/photo.model.js
@@ -1,2 +1,6 @@
-m
-s
+import mongoose from "mongoose";
+const PhotoSchema=new schema({
+title:String,
+ImgUrl:String 
+},{Timestamp:true})
+export const photo=model('photo',schema)
